be240011c444b02508eb82aa26c37a6e25a8100f
pawan yadav <pawanyadavsultania@gmail.com>
2016-08-20 18:11:14 -0700

    added spring and filter books by rating

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ListingBooksTest < ActionDispatch::IntegrationTest
 	setup do
 		Book.create(title: "programmatic programmer", rating: 5)
-		Book.create(title: "grands", rating: 5)
+		Book.create(title: "grands", rating: 4)
 	end
 
 	test "listing books" do
@@ -14,6 +14,14 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 		assert_equal Mime::JSON, response.content_type
 		assert_equal Book.count, JSON.parse(response.body).size 
 
+	end
+	test "top rated books using filters" do
+		get "/books?rating=5"
+
+		assert_equal 200, response.status
+		assert_equal Mime::JSON, response.content_type
+
+		assert_equal 1, JSON.parse(response.body).size
 	end
   
 
