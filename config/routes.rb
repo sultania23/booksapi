8d63c9e8d784b2df067b0e83232c779b4acfde86
pawan yadav <pawanyadavsultania@gmail.com>
2016-08-20 22:27:37 -0700

    added end point for books which has been read

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :books, except: [:destroy, :update]
+  resources :finished_books
 
   # Example resource route with options:
   #   resources :products do
